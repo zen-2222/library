a5038ece28cd407e4ab128315e5786390a19cfc5
M <233329682+zen-2222@users.noreply.github.com>
2026-05-02 17:30:11 +0300

    class'd book

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 //display the stuff, and implement remove button
 //also do the read button
 //book has id,name,author,pages,isread?
-function Book(name,author,pages,isread,id=crypto.randomUUID()){
-    this.id=id;
-    this.name=name;
-    this.author=author;
-    this.pages=parseInt(pages)
-    this.isread=isread;
+class Book{
+    constructor(name,author,pages,isread,id=crypto.randomUUID()){
+        this.id=id
+        this.name=name
+        this.author=author
+        this.pages=parseInt(pages)
+        this.isread=Boolean(isread)
+
+
+    }
 }
 const books=[];
 
